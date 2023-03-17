fd696aa642e34efe7c4ea0f0e33768ca9ea37c1a
ldesierra <83076120+ldesierra@users.noreply.github.com>
2023-03-16 21:58:43 -0300

    Initial gem configuration

---
+++ app/javascript/navbar.js
@@ -0,0 +1,5 @@
+window.addEventListener('load', function() {
+  document.querySelector('.navbar-toggler').addEventListener('click', function() {
+    document.getElementById('navbar-content').classList.toggle('show');
+  })
+});
